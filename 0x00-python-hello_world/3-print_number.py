a246c2f20c5d37a457aad6f24ad8a58d2c684582
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2023-08-10 05:00:54 -0700

    task0x00

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+x = 98
+print(f"{x:d} Battery street")
---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+wordf3 = word[:3]
+wordl2 = word[-2:]
+mid = word[1:-1]
+print("First 3 letters: {}".format(wordf3))
+print("Last 2 letters: {}".format(wordl2))
+print("Middle word: {}".format(mid))
